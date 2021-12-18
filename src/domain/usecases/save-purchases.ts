7cc3ece503f078531af9db1e495dcc8070a5fc34
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-12-18 16:38:37 -0300

    add LoadPurchases usecase

--- src/domain/usecases/save-purchases.ts
+++ src/domain/usecases/save-purchases.ts
@@ -1,11 +1,9 @@
-export namespace SavePurchases {
-  export type Params = {
-    id: string
-    date: Date,
-    value: number
-  }
-}
+import { PurchaseModel } from '@/domain/models'
 
 export interface SavePurchases {
   save: (purchases: Array<SavePurchases.Params>) => Promise<void>
+}
+
+export namespace SavePurchases {
+  export type Params = PurchaseModel
 }
